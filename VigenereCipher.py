ccbb580fc16f7bb1ade3188a1a22299fcab34eee
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-17 10:01:23 +0800

    encryption starts: input is now converting to numbers

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -5,7 +5,18 @@ user_input = (input("Enter a message: "))
 keyword = (input("Enter a key: "))
 
 # user's input to be encrypt through...
+def encrypt(text, key):
+    letters = list("abcdefghijklmnopqrstuvwxyz")
+    key_list = []
+
 #   converting it to number
+    for i in range(len(text)):
+        for i in key:
+            if (len(key_list) == len(text)):
+                break
+            else:
+                key_list.append(letters.index(i))
+                
 #   adding the input message and key
 #   using modulus
 
