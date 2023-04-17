51f5bfa5fd1c343abcdef43f2228890913aa0636
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-17 09:53:43 +0800

    Pseudocode

---
+++ VigenereCipher.py
@@ -0,0 +1,12 @@
+# Dahan, Regine Fae M. (BSCPE 1-5) Vigenére Cipher
+
+# ask the user for input message and key
+
+# user's input to be encrypt through...
+#   converting it to number
+#   adding the input message and key
+#   using modulus
+
+# display the output
+
+
